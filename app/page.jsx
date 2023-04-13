17411e8a56454d81753d27e57e9abf6884d49386
jcicaro <jc.icaro@gmail.com>
2023-04-13 10:42:46 +0000

    Update page.jsx 0842

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to JC Icaro's Portfolio!!</h1>
+      <h1>Welcome to JC Icaro's Portfolio!!!!!!!!!!</h1>
     </div>
   );
 };
